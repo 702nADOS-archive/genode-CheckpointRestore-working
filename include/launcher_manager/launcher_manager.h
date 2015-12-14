c2303cf6a2ad59e48000d65c6d88e3ecbcdcbf0e
Daniel Sudmann <suddani@gmail.com>
2015-12-14 13:07:54 +0100

    added lib header

---
+++ include/launcher_manager/launcher_manager.h
@@ -0,0 +1,28 @@
+#ifndef _INCLUDE__LAUNCHER_MANAGER_CONNECTION_H_
+#define _INCLUDE__LAUNCHER_MANAGER_CONNECTION_H_
+
+#include <launcher_manager/session/connection.h>
+
+namespace LauncherManager {
+  void checkpoint(void)
+  {
+
+  }
+
+  void restore(void)
+  {
+
+  }
+
+  void fork_trampoline()
+  {
+
+  }
+
+  int fork(void)
+  {
+
+  }
+}
+
+#endif

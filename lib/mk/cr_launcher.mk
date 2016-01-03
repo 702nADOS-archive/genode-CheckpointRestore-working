3d0e2e78e5875c32ece58adf756a4b596f6c386d
Daniel Sudmann <suddani@gmail.com>
2016-01-03 21:40:43 +0100

    refactor

--- lib/mk/cr_launcher.mk
+++ lib/mk/cr_launcher.mk
@@ -6,7 +6,11 @@ SRC_CC = 	manager.cc \
 					services/rm_service.cc \
 					services/rm_session_component.cc \
 					rm_registry.cc
-INC_DIR  += $(REP_DIR)/include/launcher $(REP_DIR)/src/launcher
+
+INC_DIR  += $(REP_DIR)/include/launcher \
+						$(REP_DIR)/src/launcher \
+						$(REP_DIR)/src/launcher/include
+
 CC_OPT += -fpermissive -w
 
 vpath % $(REP_DIR)/src/launcher

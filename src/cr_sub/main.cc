10dc837676707560dc089ebd2c04724bff776b4d
Michael Kubitza <khespo@gmail.com>
2016-07-05 12:05:30 +0200

    Add Fibonacci function for testing purpose

--- src/cr_sub/main.cc
+++ src/cr_sub/main.cc
@@ -1,18 +1,50 @@
 #include <base/printf.h>
 #include <timer_session/connection.h>
 
+int fib(int n)
+{
+  int first = 0;
+  int second = 1;
+  int result;
+
+  if (n == 1)
+  {
+    result = 0;
+  }
+  else if (n == 2)
+  {
+    result = 1;
+  }
+  else
+  {
+    for (int i = 2; i < n; i++)
+    {
+      result = first + second;
+      first = second;
+      second = result;
+      Genode::printf("INTERMEDIATE RESULT: %i\n", result);
+    }
+
+    return result;
+  }
+}
+
 int main(int argc, char const *argv[]) {
   Genode::printf("Hello world from cr_sub\n");
   Genode::printf("it actually works\n");
 
-  Timer::Connection timer;
-  int counter = 0;
+//  Timer::Connection timer;
+//  int counter = 0;
 
-  while(1)
-  {
-    Genode::printf("Hey i am a child at: %i\n", counter);
-    counter++;
-    timer.msleep(200);
-  }
+  int fib_result = fib(100);
+  Genode::printf("Fib(1000000000): %i\n", fib_result);
+
+//  while(1)
+//  {
+//
+//    Genode::printf("Hey i am a child at: %i\n", counter);
+//    counter++;
+//    timer.msleep(200);
+//  }
   return 0;
 }
